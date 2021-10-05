af6e23505aadee89bac2cf7ab7c29db88f048385
yuhueilee <ylee0026@student.monash.edu>
2021-10-05 14:42:38 +0800

    - Update the parameter type for threshold - Add function to generate random float value - Add while loop to generate random sea water column height

--- Assignment2/sensor_node.c
+++ Assignment2/sensor_node.c
@@ -10,13 +10,22 @@ This file contains function for simulating wireless sensor node
 #include <unistd.h>
 #include <stdbool.h>
 
-void sensor_node(int num_rows, int num_cols, double threshold, MPI_Comm world_comm, MPI_Comm nodes_comm) {
+#define CYCLE 5 // cycle for sea water column height generation
+#define LOWER_BOUND 5500.0 // lower bound for sea water column height
+#define UPPER_BOUND 6500.0 // upper bound for sea water column height
+
+/* Function prototype */
+float rand_float(unsigned int seed, float min, float max);
+
+/* Wireless sensor node simulation */
+void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_comm, MPI_Comm nodes_comm) {
+    bool terminate = 0; // set terminate to false initially
     int i, my_rank, my_cart_rank;
-    MPI_Comm cart_comm;
     int ndims = 2, reorder = 1, ierr = 0;
     int dims[ndims];
     int coord[ndims];
     int wrap_around[ndims];
+    MPI_Comm cart_comm;
 
     // assign rows and cols to dims array
     dims[0] = num_rows;
@@ -42,5 +51,25 @@ void sensor_node(int num_rows, int num_cols, double threshold, MPI_Comm world_co
     
     printf("Cart rank: %d; Cart Coord: (%d, %d);\n", my_cart_rank, coord[0], coord[1]);
     
+    sleep(my_rank); // this is to have different seed value for random float generator
+
+    int counter = 0;
+    /* Iterations */
+    do {
+        /* Generate random sea value */
+        unsigned int seed = time(NULL); // seed value to generate different random value for each process
+        float rand_sea_height = rand_float(seed, LOWER_BOUND, UPPER_BOUND);
+        printf("Rank %d. Random sea level: %.2f\n", my_rank, rand_sea_height);
+        
+        sleep(CYCLE);
+        counter++;
+    } while (counter < 10);
+    
     MPI_Comm_free(&cart_comm);
+}
+
+/* Random float value generator */
+float rand_float(unsigned int seed, float min, float max) {
+    float rand_float = (float)(rand_r(&seed) % (int)(max - min) + min);
+    return rand_float;
 }
