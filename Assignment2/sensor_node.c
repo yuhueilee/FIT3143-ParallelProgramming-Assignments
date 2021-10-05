ce53cfca65d4cdd33d920205ded8bf858a05eb54
yuhueilee <ylee0026@student.monash.edu>
2021-10-05 11:26:53 +0800

    main.c - Change if-else to switch case - Add statement to split communicator into two
    sensor_node.c
    - Change the parameters
    - Add statement to create cartesian topology using the nodes_comm
    
    simulations.h
    - Update the function prototype for sensor_node

--- Assignment2/sensor_node.c
+++ Assignment2/sensor_node.c
@@ -10,6 +10,39 @@ This file contains function for simulating wireless sensor node
 #include <unistd.h>
 #include <stdbool.h>
 
-void sensor_node(int my_rank, int my_cart_rank, MPI_Comm master_comm, MPI_Comm cart_comm) {
-    printf("My rank: %d. My cart rank: %d.\n", my_rank, my_cart_rank);
+void sensor_node(int num_rows, int num_cols, double threshold, MPI_Comm world_comm, MPI_Comm nodes_comm) {
+    int i, my_rank, my_cart_rank;
+    MPI_Comm cart_comm;
+    int ndims = 2, reorder = 1, ierr = 0;
+    int dims[ndims];
+    int coord[ndims];
+    int wrap_around[ndims];
+
+    // assign rows and cols to dims array
+    dims[0] = num_rows;
+    dims[1] = num_cols;
+
+    /* Store the rank number from nodes_comm */
+    MPI_Comm_rank(nodes_comm, &my_rank);
+
+    /* Set periodic shift to false by initializing
+    wrap_around array to 0 */
+    for (i = 0; i < ndims; i++) {
+        wrap_around[i] = 0;
+    }
+
+    /* Create cartesian topology using nodes communicator */    
+    ierr = MPI_Cart_create(nodes_comm, ndims, dims, wrap_around, reorder, &cart_comm);
+    if(ierr != 0) {
+        printf("ERROR[%d] creating 2D CART\n", ierr);
+    }
+    
+    // find my coordinates in the cartesian communicator group
+    MPI_Cart_coords(cart_comm, my_rank, ndims, coord); // coordinated is returned into the coord array
+    // use my cartesian coordinates to find my rank in cartesian group
+    MPI_Cart_rank(cart_comm, coord, &my_cart_rank);
+    
+    printf("Cart rank: %d; Cart Coord: (%d, %d);\n", my_cart_rank, coord[0], coord[1]);
+    
+    MPI_Comm_free(&cart_comm);
 }
--- Assignment2/simulations.h
+++ Assignment2/simulations.h
@@ -2,6 +2,6 @@
 #define simulations_h
 
 /* prototypes */
-void sensor_node(int my_rank, int my_cart_rank, MPI_Comm master_comm, MPI_Comm cart_comm);  // function declaration for sensor node simulation
+void sensor_node(int num_rows, int num_cols, double threshold, MPI_Comm world_comm, MPI_Comm cart_comm);  // function declaration for sensor node simulation
 
 #endif // simulations_h
