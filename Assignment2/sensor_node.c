51508a94eb340528dde2e77ae050074202cc8221
yuhueilee <ylee0026@student.monash.edu>
2021-10-07 12:13:28 +0800

    - Declare all local variables in each thread before the while loop - Rename the MPI_Statue type variables - Add local variable for sea moving average and terminate variable - Replace all shared variable with local variable - Lock the shared variable whenever access/modif

--- Assignment2/sensor_node.c
+++ Assignment2/sensor_node.c
@@ -27,8 +27,7 @@ float rand_float(unsigned int seed, float min, float max);
 
 /* Wireless sensor node simulation */
 void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_comm, MPI_Comm nodes_comm) {
-    bool terminate = 0; // set terminate to false initially
-    bool notification = 0; // set the notification to send report to base station to false initially
+    int g_terminate = 0; // set terminate to false initially
     int i, my_rank, my_cart_rank;
     int num_nbrs = 4;
     int ndims = 2, reorder = 1, ierr = 0;
@@ -36,7 +35,7 @@ void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_com
     int index = 0; // pointer to the array storing sea values
     int window_size = 5; // size of the array storing sea values
     float *p_sea_array = calloc(window_size, sizeof(float)); // initialize the array
-    float sea_moving_avg = 0.0;
+    float g_sea_moving_avg = 0.0;
     float p_recv_vals[4] = { -1.0, -1.0, -1.0, -1.0 };
     MPI_Comm cart_comm;
 
@@ -83,8 +82,15 @@ void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_com
             /* Create request and status array */
             MPI_Request p_req[num_nbrs];
             MPI_Status p_status[num_nbrs];
+            MPI_Status probe_status;
+            int j;
+            int flag; // placeholder to indicate a message is received or not
+            int msg;
+            int l_terminate = 0; // local terminiate variable
+            int count; // count the number of matched SMA
+            float sum;
+            float l_sea_moving_avg = 0.0; // local SMA variable
 
-            int counter = 0;
             do {
                 /* STEP 1: Generate random sea value */
                 unsigned int seed = time(NULL); // seed value to generate different random value for each process
@@ -96,16 +102,18 @@ void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_com
                 
                 // check if the array is filled up with values
                 if (p_sea_array[index] != 0.0) {
-                    float sum = 0.0;
-                    for (i = 0; i < window_size; i++) {
+                    sum = 0.0;
+                    for (j = 0; j < window_size; j++) {
                         sum += p_sea_array[i];
                     }
                     /* Calculate the new SMA */
                     #pragma omp critical 
-                    sea_moving_avg = sum / window_size; // lock the shared variable
+                    g_sea_moving_avg = sum / window_size; // lock the shared variable
+                    #pragma omp critical 
+                    l_sea_moving_avg = g_sea_moving_avg; // assign shared SMA to local SMA
 
                     // check if the SMA exceeds the threshold
-                    if (sea_moving_avg > threshold) {
+                    if (l_sea_moving_avg > threshold) {
                         /* Non-blocking send request to all neighbors with tag 1 */
                         for (i = 0; i < num_nbrs; i++) {
                             int request = 1;
@@ -114,7 +122,7 @@ void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_com
                         MPI_Waitall(num_nbrs, p_req, p_status);
                         
                         /* Non-blocking receive SMA from all neighbors with tag 1 */
-                        for (i = 0; i < num_nbrs; i++) {
+                        for (j = 0; j < num_nbrs; j++) {
                             MPI_Irecv(&p_recv_vals[i], 1, MPI_FLOAT, p_nbrs[i], 1, cart_comm, &p_req[i]);
                         }
                         MPI_Waitall(num_nbrs, p_req, p_status);
@@ -122,52 +130,73 @@ void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_com
                         printf("Cart rank: %d; Received top: %.2f; bottom: %.2f; left: %.2f; right: %.2f;\n", my_rank, p_recv_vals[0], p_recv_vals[1], p_recv_vals[2], p_recv_vals[3]);
                         
                         /* STEP 2: Compare SMA between neighbors */
-                        int count = 0;
-                        for (i = 0; i < num_nbrs; i++) {
-                            float range = fabs(p_recv_vals[i] - sea_moving_avg);
+                        count = 0;
+                        for (j = 0; j < num_nbrs; j++) {
+                            float range = fabs(p_recv_vals[i] - g_sea_moving_avg);
                             if (p_recv_vals[i] != -1 && range <= RANGE) {
                                 count += 1;
                             }
                         }
                         // set notification to true when at least two neighbors have similar SMA
                         if (count >= 2) {
-                            notification = 1;
                             printf("Cart rank: %d; Matched occur;\n", my_rank);
-                            notification = 0;
                         }
                     }
                 }
                 
+                /* STEP 3: Listen to base station */
+                MPI_Iprobe(0, 0, world_comm, &flag, &probe_status);
+                
+                // receive message from base station if flag is true
+                if (flag) {
+                    MPI_Recv(&msg, 1, MPI_INT, probe_status.MPI_SOURCE, 0, world_comm, &probe_status);
+                    #pragma omp critical 
+                    g_terminate = msg;
+                }
+
+                /* Retrieve terminate value from shared variable */
+                #pragma omp critical
+                l_terminate = g_terminate;
+
                 sleep(CYCLE);
-                counter++;
-            } while (counter < 10);
+            } while (! l_terminate);
         }
         /* 
             1. Listen to neighbors and send its own SMA
          */
         #pragma omp section 
         {
-            int counter = 0;
+            MPI_Status probe_status;
+            int req_msg;
+            int flag = 0; // placeholder to indicate a message is received or not
+            int l_terminate = 0; // local terminiate variable
+            float l_sea_moving_avg = 0.0; // local SMA variable
+
             do {
-                printf("Cart rank %d; Sea moving average: %.2f;\n", my_rank, sea_moving_avg);
-                MPI_Status status, temp_status;
-                int temp;
-                int flag = 0; // placeholder to indicate a message is received or not
+
+                #pragma omp critical 
+                l_sea_moving_avg = g_sea_moving_avg; // assign shared SMA to local SMA
+
+                printf("Cart rank %d; Sea moving average: %.2f;\n", my_rank, l_sea_moving_avg);
                 
                 /* Non-blocking test for a message */
-                MPI_Iprobe(MPI_ANY_SOURCE, 1, cart_comm, &flag, &status);
+                MPI_Iprobe(MPI_ANY_SOURCE, 1, cart_comm, &flag, &probe_status);
 
                 // send back SMA if flag is true
                 if (flag) {
-                    MPI_Recv(&temp, 1, MPI_INT, status.MPI_SOURCE, 1, cart_comm, &temp_status);
-                    printf("Cart rank %d sends SMA %.2f.\n", my_rank, sea_moving_avg);
+                    flag = 0;
+                    MPI_Recv(&req_msg, 1, MPI_INT, probe_status.MPI_SOURCE, 1, cart_comm, MPI_STATUS_IGNORE);
+                    printf("Cart rank %d sends SMA %.2f.\n", my_rank, l_sea_moving_avg);
                     /* Blocking send the SMA to requester */
-                    MPI_Send(&sea_moving_avg, 1, MPI_FLOAT, status.MPI_SOURCE, 1, cart_comm);
+                    MPI_Send(&l_sea_moving_avg, 1, MPI_FLOAT, probe_status.MPI_SOURCE, 1, cart_comm);
                 }
-                
-                sleep(CYCLE);
-                counter++;
-            } while (counter < 10);
+
+                /* Retrieve terminate value from shared variable */
+                #pragma omp critical
+                l_terminate = g_terminate;
+
+                sleep(1);
+            } while (! l_terminate);
         }
     }
     
