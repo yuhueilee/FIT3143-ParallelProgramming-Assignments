362e83bc0ace74710b4e9c8ac85acff67668f9ac
yuhueilee <ylee0026@student.monash.edu>
2021-10-11 11:48:16 +0800

    Add function declaration in simulations.h file

--- Assignment2/simulations.h
+++ Assignment2/simulations.h
@@ -3,5 +3,6 @@
 
 /* prototypes */
 void sensor_node(int num_rows, int num_cols, float threshold, MPI_Comm world_comm, MPI_Comm cart_comm);  // function declaration for sensor node simulation
+int base_station(int threshold, int max_iteration, MPI_Comm world_comm);
 
 #endif // simulations_h
