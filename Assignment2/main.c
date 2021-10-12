016ba1af6f78622e1f044979ba8382d9c9ac8ce3
yuhueilee <ylee0026@student.monash.edu>
2021-10-12 12:40:39 +0800

    Update the main C file

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -9,10 +9,12 @@
 #include <stdbool.h>
 #include "simulations.h" // simulations header file
 
+#define MAX_RANGE 500.0
+
 int main(int argc, char *argv[]) {
 
     /* Variables declaration */
-    int my_rank, size, num_rows, num_cols, provided;
+    int my_rank, size, num_rows, num_cols, max_iterations, provided;
     float threshold;
     MPI_Comm nodes_comm;
 
@@ -30,18 +32,27 @@ int main(int argc, char *argv[]) {
     MPI_Comm_split(MPI_COMM_WORLD, my_rank > 0, 0, &nodes_comm);
 
     /* Initialize number of rows, cols and height of threshold */
-    if (argc == 4) {
+    if (argc == 5) {
         num_rows = atoi(argv[1]);
         num_cols = atoi(argv[2]);
         threshold = atof(argv[3]);
+        max_iterations = atof(argv[4]);
         // check if number of processes matches the value of rows * cols
         if (num_rows * num_cols != (size - 1)) {
-            if(my_rank == 0) printf("ERROR: rows(%d) * cols(%d) != size - 1(%d)", num_rows, num_cols, size - 1);
+            if(my_rank == 0) printf("ERROR: rows(%d) * cols(%d) != size - 1(%d).\n", num_rows, num_cols, size - 1);
             MPI_Finalize(); 
             return 0; 
+        } else if (threshold < MAX_RANGE) {
+            if(my_rank == 0) printf("ERROR: threshold (%.2f) is less than the minimum boundary (%.2f).\n", threshold, MAX_RANGE);
+            MPI_Finalize(); 
+            return 0;
+        } else if (max_iterations < 1) {
+            if(my_rank == 0) printf("ERROR: max_iterations (%d) has to be at least 1.\n", max_iterations);
+            MPI_Finalize(); 
+            return 0;
         }
     } else {
-        if(my_rank == 0) printf("ERROR: Number of arguments is not 3.");
+        if(my_rank == 0) printf("ERROR: Number of arguments is not 4.\n");
         MPI_Finalize(); 
         return 0;       
     }
@@ -49,9 +60,18 @@ int main(int argc, char *argv[]) {
     /* Switch case to call different function based on rank number */
     switch (my_rank) {
         case 0: {
+            // create a sentinel file for user to terminate the program
+            char *p_sentinel_name = "sentinel.txt";
+            FILE *pSentinelFile = fopen(p_sentinel_name, "w");
+            fprintf(pSentinelFile, "0");
+            fclose(pSentinelFile);
+
+            // print statement for user to terminate the program
+            printf("To terminate the program, change the value to 1 in sentinel.txt file.\n");
             printf("Root Rank: %d. Comm Size: %d. Grid Dimension = [%d x %d]. Threshold: %f.\n", my_rank, size, num_rows, num_cols, threshold);
+            
             /* Base station */
-            base_station(threshold, 25, MPI_COMM_WORLD);
+            base_station(p_sentinel_name, threshold, max_iterations, MPI_COMM_WORLD);
             break;
         }
         default: {
